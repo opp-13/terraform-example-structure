d88bd6a8707845c6adb4568aced30b0a8bf69abe
Sungho, Kim <john_sungho@naver.com>
2026-08-16 15:57:15 +0900

    fix: 버킷 이름 수정
    이름 겹쳐서 생성 안되는 부분 수정

--- init/bootstrap/variables.tf
+++ init/bootstrap/variables.tf
@@ -1,7 +1,7 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket that will hold Terraform remote state for this project"
   type        = string
-  default     = "terraform-state-example"
+  default     = "terraform-state-example-s6john"
 }
 
 variable "aws_profile" {
